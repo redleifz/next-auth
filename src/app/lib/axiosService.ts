8a323fa1a91f66a5d76e6ba59aa7f27fb253129b
Jongjate Choomjairug <jongjate@isoftel.co.th>
2023-11-02 17:33:35 +0700

    axios service

--- src/app/lib/axiosService.ts
+++ src/app/lib/axiosService.ts
@@ -3,23 +3,21 @@ import { getSession } from 'next-auth/react'
 
 const baseURL = process.env.NEXT_PUBLIC_ENDPOINT_1 + 'api/'
 const axiosService = axios.create({
-  baseURL,
+    baseURL,
 })
 
 axiosService.interceptors.request.use(
-  async (config) => {
-    const session = await getSession()
-    console.log(session)
-   
-    if (session) {
-      config.headers['Authorization'] = `Bearer ${session.user.accessToken}`
-    }
-
-    return config
-  },
-  (error) => {
-    return Promise.reject(error)
-  },
+    async (config) => {
+        const session = await getSession()
+        // console.log(session)
+        if (session) {
+            config.headers['Authorization'] = `Bearer ${session.user.accessToken}`
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error)
+    },
 )
 
 export default axiosService
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -4,15 +4,14 @@ import { signOut, useSession } from "next-auth/react";
 
 const AppBar = () => {
   const { data: session, status } = useSession();
-  // console.log(session);
 
   return (
     <>
       <div className="flex justify-between bg-[#3A4D39] text-[#ECE3CE] ">
         <div className="h-10 pl-3   flex items-center gap-3">
           <Link href={`/`}>App Logo</Link>
-          <Link href={`/admin`}>Admin</Link>
-          <Link href={`/profile`}>Profile</Link>
+          {session?.user.role === `admin` && <Link href={`/admin`}>Admin</Link>}
+          {session?.user.role && <Link href={`/profile`}>Profile</Link>}
         </div>
 
         <div className="h-10 text-white flex items-center mr-3 ">
